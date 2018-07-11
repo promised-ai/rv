194fcc23e2f87224e7ced4b9282ecd627fab3b1f
Baxter Eaves <baxyjr@gmail.com>
2018-07-10 21:28:07 -0500

    Add beta tests

--- src/dist/beta.rs
+++ src/dist/beta.rs
@@ -39,8 +39,8 @@ impl Rv for Beta {
     type DatumType = f64;
 
     fn ln_f(&self, x: &f64) -> f64 {
-        let denom = self.alpha.ln_beta(self.beta);
-        x.powf(self.alpha - 1.0) * (1.0 - *x).powf(self.beta - 1.0) - denom
+        (self.alpha - 1.0) * x.ln() + (self.beta - 1.0) * (1.0 - *x).ln()
+            - self.alpha.ln_beta(self.beta)
     }
 
     fn draw<R: Rng>(&self, rng: &mut R) -> f64 {
@@ -111,7 +111,7 @@ impl Entropy for Beta {
         let apb = self.alpha + self.beta;
         self.alpha.ln_beta(self.beta)
             - (self.alpha - 1.0) * self.alpha.digamma()
-            - (self.beta - 1.0) * self.beta.digamma() + (apb + 2.0) * apb.digamma()
+            - (self.beta - 1.0) * self.beta.digamma() + (apb - 2.0) * apb.digamma()
     }
 }
 
@@ -119,7 +119,7 @@ impl Skewness for Beta {
     fn skewness(&self) -> Option<f64> {
         let apb = self.alpha + self.beta;
         let numer = 2.0 * (self.beta - self.alpha) * (apb + 1.0).sqrt();
-        let denom = self.alpha * self.beta * (apb + 2.0) * (apb + 3.0);
+        let denom = (apb + 2.0) * (self.alpha * self.beta).sqrt();
         Some(numer / denom)
     }
 }
@@ -132,5 +132,153 @@ impl Kurtosis for Beta {
         let numer = 6.0 * (amb * amb * (apb + 1.0) - atb * (apb + 2.0));
         let denom = atb * (apb + 2.0) * (apb + 3.0);
         Some(numer / denom)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    extern crate assert;
+    use super::*;
+    use std::f64;
+
+    const TOL: f64 = 1E-12;
+
+    #[test]
+    fn new() {
+        let beta = Beta::new(1.0, 2.0);
+        assert::close(beta.alpha, 1.0, TOL);
+        assert::close(beta.beta, 2.0, TOL);
+    }
+
+    #[test]
+    fn uniform() {
+        let beta = Beta::uniform();
+        assert::close(beta.alpha, 1.0, TOL);
+        assert::close(beta.beta, 1.0, TOL);
+    }
+
+    #[test]
+    fn jeffreys() {
+        let beta = Beta::jeffreys();
+        assert::close(beta.alpha, 0.5, TOL);
+        assert::close(beta.beta, 0.5, TOL);
+    }
+
+    #[test]
+    fn ln_pdf_center_value() {
+        let beta = Beta::new(1.5, 2.0);
+        assert::close(beta.ln_pdf(&0.5), 0.28203506914240184, TOL);
+    }
+
+    #[test]
+    fn ln_pdf_low_value() {
+        let beta = Beta::new(1.5, 2.0);
+        assert::close(beta.ln_pdf(&0.01), -0.99087958886522731, TOL);
+    }
+
+    #[test]
+    fn ln_pdf_high_value() {
+        let beta = Beta::new(1.5, 2.0);
+        assert::close(beta.ln_pdf(&0.99), -3.2884395139325218, TOL);
+    }
+
+    #[test]
+    fn draw_should_resturn_values_within_0_to_1() {
+        let mut rng = rand::thread_rng();
+        let beta = Beta::jeffreys();
+        for _ in 0..100 {
+            let x = beta.draw(&mut rng);
+            assert!(0.0 < x && x < 1.0);
+        }
+    }
+
+    #[test]
+    fn sample_returns_the_correct_number_draws() {
+        let mut rng = rand::thread_rng();
+        let beta = Beta::jeffreys();
+        let xs = beta.sample(103, &mut rng);
+        assert_eq!(xs.len(), 103);
+    }
+
+    #[test]
+    fn uniform_mean() {
+        assert::close(Beta::uniform().mean().unwrap(), 0.5, TOL);
+    }
+
+    #[test]
+    fn jeffreys_mean() {
+        assert::close(Beta::jeffreys().mean().unwrap(), 0.5, TOL);
+    }
+
+    #[test]
+    fn mean() {
+        assert::close(Beta::new(1.0, 5.0).mean().unwrap(), 1.0 / 6.0, TOL);
+    }
+
+    #[test]
+    fn variance() {
+        let beta = Beta::new(1.5, 2.0);
+        assert::close(beta.variance().unwrap(), 0.054421768707482991, TOL);
+    }
+
+    #[test]
+    fn mode_for_alpha_and_beta_greater_than_one() {
+        let beta = Beta::new(1.5, 2.0);
+        assert::close(beta.mode().unwrap(), 0.5 / 1.5, TOL);
+    }
+
+    #[test]
+    fn mode_for_alpha_one_and_large_beta() {
+        let beta = Beta::new(1.0, 2.0);
+        assert::close(beta.mode().unwrap(), 0.0, TOL);
+    }
+
+    #[test]
+    fn mode_for_large_alpha_and_beta_one() {
+        let beta = Beta::new(2.0, 1.0);
+        assert::close(beta.mode().unwrap(), 1.0, TOL);
+    }
+
+    #[test]
+    fn mode_for_alpha_less_than_one_is_none() {
+        assert!(Beta::new(0.99, 2.0).mode().is_none());
+    }
+
+    #[test]
+    fn mode_for_beta_less_than_one_is_none() {
+        assert!(Beta::new(2.0, 0.99).mode().is_none());
+    }
+
+    #[test]
+    fn mode_for_alpha_and_beta_less_than_one_is_none() {
+        assert!(Beta::new(0.99, 0.99).mode().is_none());
+    }
+
+    #[test]
+    fn entropy() {
+        let beta = Beta::new(1.5, 2.0);
+        assert::close(beta.entropy(), -0.10805020110232236, TOL);
+    }
+
+    #[test]
+    fn uniform_skewness_should_be_zero() {
+        assert::close(Beta::uniform().skewness().unwrap(), 0.0, TOL);
+    }
+
+    #[test]
+    fn jeffreysf_skewness_should_be_zero() {
+        assert::close(Beta::jeffreys().skewness().unwrap(), 0.0, TOL);
+    }
+
+    #[test]
+    fn skewness() {
+        let beta = Beta::new(1.5, 2.0);
+        assert::close(beta.skewness().unwrap(), 0.22268088570756162, TOL);
+    }
+
+    #[test]
+    fn kurtosis() {
+        let beta = Beta::new(1.5, 2.0);
+        assert::close(beta.kurtosis().unwrap(), -0.8601398601398601, TOL);
     }
 }
