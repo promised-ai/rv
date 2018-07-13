578e16d42294cefaae32dfc8b54104e6349f2a6b
Baxter Eaves <baxyjr@gmail.com>
2018-07-12 23:29:43 -0500

    Add utils file

---
+++ src/utils.rs
@@ -0,0 +1,105 @@
+extern crate rand;
+
+use self::rand::distributions::Open01;
+use self::rand::Rng;
+
+/// Safely compute `log(sum(exp(xs))`
+pub fn logsumexp(xs: &[f64]) -> f64 {
+    if xs.is_empty() {
+        panic!("Empty container");
+    } else if xs.len() == 1 {
+        xs[0]
+    } else {
+        let maxval =
+            *xs.iter().max_by(|x, y| x.partial_cmp(y).unwrap()).unwrap();
+
+        xs.iter().fold(0.0, |acc, x| acc + (x - maxval).exp()).ln() + maxval
+    }
+}
+
+/// Draw a `usize` from the categorical distribution defined by `ln_weights`.
+/// Assumes that the sum of the weights is 0 (the sum of `exp(ln_weights)` is
+/// 1).
+pub fn ln_pflip<R: Rng>(
+    ln_weights: &[f64],
+    n: usize,
+    rng: &mut R,
+) -> Vec<usize> {
+    let mut cdf: Vec<f64> = ln_weights.iter().map(|w| w.exp()).collect();
+
+    // doing this instead of calling pflip shaves about 30% off the runtime.
+    for i in 1..cdf.len() {
+        cdf[i] += cdf[i - 1];
+    }
+
+    (1..n)
+        .map(|_| {
+            let r = rng.sample(Open01);
+            cdf.iter()
+                .position(|&w| w > r)
+                .expect(format!("Could not draw from {:?}", cdf).as_str())
+        })
+        .collect()
+}
+
+pub fn argmax<T: PartialOrd>(xs: &[T]) -> usize {
+    if xs.is_empty() {
+        panic!("Empty container");
+    }
+
+    if xs.len() == 1 {
+        0
+    } else {
+        let mut maxval = &xs[0];
+        let mut max_ix: usize = 0;
+        for i in 1..xs.len() {
+            let x = &xs[i];
+            if x > maxval {
+                maxval = x;
+                max_ix = i;
+            }
+        }
+        max_ix
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    extern crate assert;
+
+    const TOL: f64 = 1E-12;
+
+    #[test]
+    fn logsumexp_on_vector_of_zeros() {
+        let xs: Vec<f64> = vec![0.0; 5];
+        // should be about log(5)
+        assert::close(logsumexp(&xs), 1.6094379124341003, TOL);
+    }
+
+    #[test]
+    fn logsumexp_on_random_values() {
+        let xs: Vec<f64> = vec![
+            0.30415386,
+            -0.07072296,
+            -1.04287019,
+            0.27855407,
+            -0.81896765,
+        ];
+        assert::close(logsumexp(&xs), 1.4820007894263059, TOL);
+    }
+
+    #[test]
+    fn logsumexp_returns_only_value_on_one_element_container() {
+        let xs: Vec<f64> = vec![0.30415386];
+        assert::close(logsumexp(&xs), 0.30415386, TOL);
+    }
+
+    #[test]
+    #[should_panic]
+    fn logsumexp_should_panic_on_empty() {
+        let xs: Vec<f64> = Vec::new();
+        logsumexp(&xs);
+    }
+
+}
