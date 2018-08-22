2385d4b85619f6fc491a9b9e7e7917444fefc76f
Baxter Eaves <baxyjr@gmail.com>
2018-08-22 18:41:24 -0500

    Cdf and docs for Mixture

--- src/dist/mixture.rs
+++ src/dist/mixture.rs
@@ -5,23 +5,47 @@ use misc::{logsumexp, pflip};
 use std::io;
 use traits::*;
 
+/// [Mixture distribution](https://en.wikipedia.org/wiki/Mixture_model)
+/// Σ w<sub>i</sub> f(x|θ<sub>i</sub>)
+///
+/// A mixture distribution is a convex combination of distributions.
+///
+/// # Example
+///
+/// A bimodal Gaussian mixture model
+///
+/// ```
+/// use rv::prelude::*;
+///
+/// let g1 = Gaussian::new(-2.5, 1.0).unwrap();
+/// let g2 = Gaussian::new(2.0, 2.1).unwrap();
+///
+/// // f(x) = 0.6 * N(-2.5, 1.0) + 0.4 * N(2.0, 2.1)
+/// let mm = Mixture::new(vec![0.6, 0.4], vec![g1, g2]).unwrap();
+/// ```
 pub struct Mixture<Fx> {
+    /// The weights for each component distribution. All entries must be
+    /// positive and sum to 1.
     pub weights: Vec<f64>,
+    /// The component distributions.
     pub components: Vec<Fx>,
 }
 
 impl<Fx> Mixture<Fx> {
     pub fn new(weights: Vec<f64>, components: Vec<Fx>) -> io::Result<Self> {
-        let weights_sum = weights.iter().fold(0.0, |acc, &w| acc + w);
+        let weights_ok = weights.iter().all(|&w| w >= 0.0)
+            && (weights.iter().fold(0.0, |acc, &w| acc + w) - 1.0).abs()
+                < 1E-12;
         let length_mismatch = weights.len() != components.len();
         if length_mismatch {
             let err_kind = io::ErrorKind::InvalidInput;
             let msg = "weights.len() != components.len()";
             let err = io::Error::new(err_kind, msg);
             Err(err)
-        } else if (weights_sum - 1.0).abs() > 1E-12 {
+        } else if !weights_ok {
             let err_kind = io::ErrorKind::InvalidInput;
-            let err = io::Error::new(err_kind, "weights must sum to 1");
+            let msg = "weights must be positive and sum to 1";
+            let err = io::Error::new(err_kind, msg);
             Err(err)
         } else {
             Ok(Mixture {
@@ -78,6 +102,18 @@ where
     }
 }
 
+impl<X, Fx> Cdf<X> for Mixture<Fx>
+where
+    Fx: Rv<X> + Cdf<X>,
+{
+    fn cdf(&self, x: &X) -> f64 {
+        self.weights
+            .iter()
+            .zip(self.components.iter())
+            .fold(0.0_f64, |acc, (&w, cpnt)| acc + w * cpnt.cdf(&x))
+    }
+}
+
 impl<X, Fx> ContinuousDistr<X> for Mixture<Fx>
 where
     Fx: Rv<X> + ContinuousDistr<X>,
@@ -126,7 +162,9 @@ where
 mod tests {
     use super::*;
     extern crate assert;
-    use dist::Gaussian;
+    use dist::{Gaussian, Poisson};
+
+    const TOL: f64 = 1E-12;
 
     #[test]
     fn new_should_not_allow_bad_weights() {
@@ -195,5 +233,134 @@ mod tests {
             .fold(0.0_f64, |acc, x: f64| acc + x)
             / 100_000.0;
         assert::close(ybar, -2.4, 0.05);
+    }
+
+    #[test]
+    fn continuous_pdf() {
+        let xs: Vec<f64> = vec![
+            -2.0,
+            -1.7105263157894737,
+            -1.4210526315789473,
+            -1.131578947368421,
+            -0.8421052631578947,
+            -0.5526315789473684,
+            -0.26315789473684204,
+            0.02631578947368407,
+            0.3157894736842106,
+            0.6052631578947372,
+            0.8947368421052633,
+            1.1842105263157894,
+            1.473684210526316,
+            1.7631578947368425,
+            2.052631578947368,
+            2.3421052631578947,
+            2.6315789473684212,
+            2.921052631578948,
+            3.2105263157894743,
+            3.5,
+        ];
+
+        let target: Vec<f64> = vec![
+            0.06473380602589335,
+            0.08497882889790014,
+            0.11217654708252275,
+            0.14501073352074248,
+            0.1797629425978142,
+            0.21067644115271195,
+            0.23139135335682923,
+            0.23702954614998323,
+            0.2260163812419922,
+            0.2007791966709858,
+            0.167275199122399,
+            0.13454608202641027,
+            0.11515152871602127,
+            0.12213065296617545,
+            0.1549752516467795,
+            0.18610946002808507,
+            0.17957278989941344,
+            0.1317193335426053,
+            0.07419043357505534,
+            0.03498448751938728,
+        ];
+
+        let components = vec![
+            Gaussian::new(0.0, 1.0).unwrap(),
+            Gaussian::new(-1.0, 3.0).unwrap(),
+            Gaussian::new(2.5, 0.5).unwrap(),
+        ];
+        let weights = vec![0.5, 0.3, 0.2];
+
+        let mm = Mixture::new(weights, components).unwrap();
+
+        let fx: Vec<f64> = xs.iter().map(|x| mm.pdf(x)).collect();
+
+        assert::close(fx, target, TOL);
+    }
+
+    #[test]
+    fn discrete_pdf() {
+        let xs: Vec<u32> = (0..20).map(|i| i as u32).collect();
+
+        let target: Vec<f64> = vec![
+            0.18944349223829393,
+            0.20600928711172717,
+            0.13638139292344745,
+            0.09077999553365237,
+            0.07005752694856147,
+            0.05598752152045468,
+            0.04412525380494339,
+            0.035914449903067774,
+            0.0314554228183013,
+            0.02899187154010165,
+            0.02660980083600876,
+            0.023324644922075175,
+            0.01914852009968446,
+            0.014640805796680027,
+            0.010432339897673344,
+            0.006948125661306361,
+            0.004340886637601523,
+            0.002553064633728986,
+            0.0014182807755675772,
+            0.0007464449417949772,
+        ];
+
+        let components = vec![
+            Poisson::new(1.0).unwrap(),
+            Poisson::new(4.0).unwrap(),
+            Poisson::new(10.0).unwrap(),
+        ];
+        let weights = vec![0.5, 0.3, 0.2];
+
+        let mm = Mixture::new(weights, components).unwrap();
+
+        let fx: Vec<f64> = xs.iter().map(|x| mm.pmf(x)).collect();
+
+        assert::close(fx, target, TOL);
+    }
+
+    #[test]
+    fn cdf_for_overlapping_gaussians() {
+        let components = vec![
+            Gaussian::standard(),
+            Gaussian::standard(),
+            Gaussian::standard(),
+        ];
+        let weights = vec![1.0 / 3.0; 3];
+        let mm = Mixture::new(weights, components).unwrap();
+
+        assert::close(mm.cdf(&0.0_f64), 0.5, TOL);
+    }
+
+    #[test]
+    fn cdf_for_symmetric_gaussian_mixture() {
+        let components = vec![
+            Gaussian::new(-1.0, 3.0).unwrap(),
+            Gaussian::new(1.0, 1.0).unwrap(),
+            Gaussian::new(3.0, 3.0).unwrap(),
+        ];
+        let weights = vec![1.0 / 3.0; 3];
+        let mm = Mixture::new(weights, components).unwrap();
+
+        assert::close(mm.cdf(&1.0_f64), 0.5, TOL);
     }
 }
