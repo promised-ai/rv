70b89b473eb87471d7b0cee6823435960f2e2da9
Baxter Eaves <baxyjr@gmail.com>
2021-02-11 21:55:34 -0600

    Add normal inverse gamma

---
+++ src/dist/normal_inv_gamma.rs
@@ -0,0 +1,336 @@
+//! A common conjugate prior for Gaussians with unknown mean and variance
+//!
+//! For a reference see section 6 of [Kevin Murphy's
+//! whitepaper](https://www.cs.ubc.ca/~murphyk/Papers/bayesGauss.pdf).
+#[cfg(feature = "serde1")]
+use serde::{Deserialize, Serialize};
+
+mod gaussian_prior;
+
+use crate::dist::{Gaussian, InvGamma};
+use crate::impl_display;
+use crate::traits::Rv;
+use rand::Rng;
+
+/// Prior for Gaussian
+///
+/// Given `x ~ N(μ, σ)`, the Normal Inverse Gamma prior implies that
+/// `μ ~ N(m, sqrt(v)σ)` and `ρ ~ InvGamma(a, b)`.
+#[derive(Debug, Clone, PartialEq)]
+#[cfg_attr(feature = "serde1", derive(Serialize, Deserialize))]
+pub struct NormalInvGamma {
+    m: f64,
+    v: f64,
+    a: f64,
+    b: f64,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+#[cfg_attr(feature = "serde1", derive(Serialize, Deserialize))]
+pub enum NormalInvGammaError {
+    /// The m parameter is infinite or NaN
+    MNotFinite { m: f64 },
+    /// The v parameter is less than or equal to zero
+    VTooLow { v: f64 },
+    /// The v parameter is infinite or NaN
+    VNotFinite { v: f64 },
+    /// The a parameter is less than or equal to zero
+    ATooLow { a: f64 },
+    /// The a parameter is infinite or NaN
+    ANotFinite { a: f64 },
+    /// The b parameter is less than or equal to zero
+    BTooLow { b: f64 },
+    /// The b parameter is infinite or NaN
+    BNotFinite { b: f64 },
+}
+
+impl NormalInvGamma {
+    /// Create a new Normal Inverse Gamma distribution
+    ///
+    /// # Arguments
+    /// - m: The prior mean
+    /// - v: Relative variance of μ versus data
+    /// - a: The mean of variance is b / (a - 1)
+    /// - b: Degrees of freedom of the variance
+    pub fn new(
+        m: f64,
+        v: f64,
+        a: f64,
+        b: f64,
+    ) -> Result<Self, NormalInvGammaError> {
+        if !m.is_finite() {
+            Err(NormalInvGammaError::MNotFinite { m })
+        } else if !v.is_finite() {
+            Err(NormalInvGammaError::VNotFinite { v })
+        } else if !a.is_finite() {
+            Err(NormalInvGammaError::ANotFinite { a })
+        } else if !b.is_finite() {
+            Err(NormalInvGammaError::BNotFinite { b })
+        } else if v <= 0.0 {
+            Err(NormalInvGammaError::VTooLow { v })
+        } else if a <= 0.0 {
+            Err(NormalInvGammaError::ATooLow { a })
+        } else if b <= 0.0 {
+            Err(NormalInvGammaError::BTooLow { b })
+        } else {
+            Ok(NormalInvGamma { m, v, a, b })
+        }
+    }
+
+    /// Creates a new NormalInvGamma without checking whether the parameters are
+    /// valid.
+    #[inline(always)]
+    pub fn new_unchecked(m: f64, v: f64, a: f64, b: f64) -> Self {
+        NormalInvGamma { m, v, a, b }
+    }
+
+    /// Returns (m, v, a, b)
+    #[inline(always)]
+    pub fn params(&self) -> (f64, f64, f64, f64) {
+        (self.m, self.v, self.a, self.b)
+    }
+
+    /// Get the m parameter
+    #[inline(always)]
+    pub fn m(&self) -> f64 {
+        self.m
+    }
+
+    /// Set the value of m
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use rv::dist::NormalInvGamma;
+    ///
+    /// let mut nig = NormalInvGamma::new(0.0, 1.2, 2.3, 3.4).unwrap();
+    /// assert_eq!(nig.m(), 0.0);
+    ///
+    /// nig.set_m(-1.1).unwrap();
+    /// assert_eq!(nig.m(), -1.1);
+    /// ```
+    ///
+    /// Will error for invalid values
+    ///
+    /// ```rust
+    /// # use rv::dist::NormalInvGamma;
+    /// # let mut nig = NormalInvGamma::new(0.0, 1.2, 2.3, 3.4).unwrap();
+    /// assert!(nig.set_m(-1.1).is_ok());
+    /// assert!(nig.set_m(std::f64::INFINITY).is_err());
+    /// assert!(nig.set_m(std::f64::NEG_INFINITY).is_err());
+    /// assert!(nig.set_m(std::f64::NAN).is_err());
+    /// ```
+    #[inline]
+    pub fn set_m(&mut self, m: f64) -> Result<(), NormalInvGammaError> {
+        if m.is_finite() {
+            self.set_m_unchecked(m);
+            Ok(())
+        } else {
+            Err(NormalInvGammaError::MNotFinite { m })
+        }
+    }
+
+    /// Set the value of m without input validation
+    #[inline(always)]
+    pub fn set_m_unchecked(&mut self, m: f64) {
+        self.m = m;
+    }
+
+    /// Get the v parameter
+    #[inline]
+    pub fn v(&self) -> f64 {
+        self.v
+    }
+
+    /// Set the value of v
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use rv::dist::NormalInvGamma;
+    ///
+    /// let mut nig = NormalInvGamma::new(0.0, 1.2, 2.3, 3.4).unwrap();
+    /// assert_eq!(nig.v(), 1.2);
+    ///
+    /// nig.set_v(4.3).unwrap();
+    /// assert_eq!(nig.v(), 4.3);
+    /// ```
+    ///
+    /// Will error for invalid values
+    ///
+    /// ```rust
+    /// # use rv::dist::NormalInvGamma;
+    /// # let mut nig = NormalInvGamma::new(0.0, 1.2, 2.3, 3.4).unwrap();
+    /// assert!(nig.set_v(2.1).is_ok());
+    ///
+    /// // must be greater than zero
+    /// assert!(nig.set_v(0.0).is_err());
+    /// assert!(nig.set_v(-1.0).is_err());
+    ///
+    ///
+    /// assert!(nig.set_v(std::f64::INFINITY).is_err());
+    /// assert!(nig.set_v(std::f64::NEG_INFINITY).is_err());
+    /// assert!(nig.set_v(std::f64::NAN).is_err());
+    /// ```
+    #[inline]
+    pub fn set_v(&mut self, v: f64) -> Result<(), NormalInvGammaError> {
+        if !v.is_finite() {
+            Err(NormalInvGammaError::VNotFinite { v })
+        } else if v <= 0.0 {
+            Err(NormalInvGammaError::VTooLow { v })
+        } else {
+            self.set_v_unchecked(v);
+            Ok(())
+        }
+    }
+
+    /// Set the value of v without input validation
+    #[inline]
+    pub fn set_v_unchecked(&mut self, v: f64) {
+        self.v = v;
+    }
+
+    /// Get the a parameter
+    #[inline]
+    pub fn a(&self) -> f64 {
+        self.a
+    }
+
+    /// Set the value of a
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use rv::dist::NormalInvGamma;
+    ///
+    /// let mut nig = NormalInvGamma::new(0.0, 1.2, 2.3, 3.4).unwrap();
+    /// assert_eq!(nig.a(), 2.3);
+    ///
+    /// nig.set_a(4.3).unwrap();
+    /// assert_eq!(nig.a(), 4.3);
+    /// ```
+    ///
+    /// Will error for invalid values
+    ///
+    /// ```rust
+    /// # use rv::dist::NormalInvGamma;
+    /// # let mut nig = NormalInvGamma::new(0.0, 1.2, 2.3, 3.4).unwrap();
+    /// assert!(nig.set_a(2.1).is_ok());
+    ///
+    /// // must be greater than zero
+    /// assert!(nig.set_a(0.0).is_err());
+    /// assert!(nig.set_a(-1.0).is_err());
+    ///
+    ///
+    /// assert!(nig.set_a(std::f64::INFINITY).is_err());
+    /// assert!(nig.set_a(std::f64::NEG_INFINITY).is_err());
+    /// assert!(nig.set_a(std::f64::NAN).is_err());
+    /// ```
+    #[inline]
+    pub fn set_a(&mut self, a: f64) -> Result<(), NormalInvGammaError> {
+        if !a.is_finite() {
+            Err(NormalInvGammaError::ANotFinite { a })
+        } else if a <= 0.0 {
+            Err(NormalInvGammaError::ATooLow { a })
+        } else {
+            self.set_a_unchecked(a);
+            Ok(())
+        }
+    }
+
+    /// Set the value of a without input validation
+    #[inline]
+    pub fn set_a_unchecked(&mut self, a: f64) {
+        self.a = a;
+    }
+
+    /// Get the b parameter
+    #[inline]
+    pub fn b(&self) -> f64 {
+        self.b
+    }
+
+    /// Set the value of b
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use rv::dist::NormalInvGamma;
+    ///
+    /// let mut nig = NormalInvGamma::new(0.0, 1.2, 2.3, 3.4).unwrap();
+    /// assert_eq!(nig.b(), 3.4);
+    ///
+    /// nig.set_b(4.3).unwrap();
+    /// assert_eq!(nig.b(), 4.3);
+    /// ```
+    ///
+    /// Will error for invalid values
+    ///
+    /// ```rust
+    /// # use rv::dist::NormalInvGamma;
+    /// # let mut nig = NormalInvGamma::new(0.0, 1.2, 2.3, 3.4).unwrap();
+    /// assert!(nig.set_b(2.1).is_ok());
+    ///
+    /// // must be greater than zero
+    /// assert!(nig.set_b(0.0).is_err());
+    /// assert!(nig.set_b(-1.0).is_err());
+    ///
+    ///
+    /// assert!(nig.set_b(std::f64::INFINITY).is_err());
+    /// assert!(nig.set_b(std::f64::NEG_INFINITY).is_err());
+    /// assert!(nig.set_b(std::f64::NAN).is_err());
+    /// ```
+    #[inline]
+    pub fn set_b(&mut self, b: f64) -> Result<(), NormalInvGammaError> {
+        if !b.is_finite() {
+            Err(NormalInvGammaError::BNotFinite { b })
+        } else if b <= 0.0 {
+            Err(NormalInvGammaError::BTooLow { b })
+        } else {
+            self.set_b_unchecked(b);
+            Ok(())
+        }
+    }
+
+    /// Set the value of b without input validation
+    #[inline(always)]
+    pub fn set_b_unchecked(&mut self, b: f64) {
+        self.b = b;
+    }
+}
+
+impl From<&NormalInvGamma> for String {
+    fn from(nig: &NormalInvGamma) -> String {
+        format!(
+            "Normal-Inverse-Gamma(m: {}, v: {}, a: {}, b: {})",
+            nig.m, nig.v, nig.a, nig.b
+        )
+    }
+}
+
+impl_display!(NormalInvGamma);
+
+impl Rv<Gaussian> for NormalInvGamma {
+    fn ln_f(&self, x: &Gaussian) -> f64 {
+        // TODO: could cache the gamma and Gaussian distributions
+        let lnf_sigma =
+            InvGamma::new_unchecked(self.a, self.b).ln_f(&x.sigma().powi(2));
+        let prior_sigma = self.v.sqrt() * x.sigma();
+        let lnf_mu = Gaussian::new_unchecked(self.m, prior_sigma).ln_f(&x.mu());
+        lnf_sigma + lnf_mu
+    }
+
+    fn draw<R: Rng>(&self, mut rng: &mut R) -> Gaussian {
+        // NOTE: The parameter errors in this fn shouldn't happen if the prior
+        // parameters are valid.
+        let var: f64 = InvGamma::new(self.a, self.b)
+            .expect("Invalid σ params when drawing Gaussian")
+            .draw(&mut rng);
+        let post_sigma: f64 = (self.v * var).sqrt();
+        let mu: f64 = Gaussian::new(self.m, post_sigma)
+            .expect("Invalid μ internal when drawing Gaussian")
+            .draw(&mut rng);
+
+        Gaussian::new(mu, var.sqrt()).expect("Invalid params")
+    }
+}
---
+++ src/dist/normal_inv_gamma/gaussian_prior.rs
@@ -0,0 +1,168 @@
+use std::f64::consts::LN_2;
+
+use special::Gamma as _;
+
+use crate::consts::{HALF_LN_PI, LN_PI};
+use crate::data::{DataOrSuffStat, GaussianSuffStat};
+use crate::dist::{Gaussian, NormalInvGamma};
+use crate::traits::*;
+
+macro_rules! extract_stat_then {
+    ($x: ident, $func: expr) => {{
+        match $x {
+            DataOrSuffStat::SuffStat(ref stat) => $func(&stat),
+            DataOrSuffStat::Data(xs) => {
+                let mut stat = GaussianSuffStat::new();
+                stat.observe_many(&xs);
+                $func(&stat)
+            }
+            DataOrSuffStat::None => {
+                let stat = GaussianSuffStat::new();
+                $func(&stat)
+            }
+        }
+    }};
+}
+
+#[inline]
+fn ln_z(v: f64, a: f64, b: f64) -> f64 {
+    -(a * b.ln() - 0.5 * v.ln() - a.ln_gamma().0)
+}
+
+// XXX: Check out section 6.3 from Kevin Murphy's paper
+// https://www.cs.ubc.ca/~murphyk/Papers/bayesGauss.pdf
+fn posterior_from_stat(
+    nig: &NormalInvGamma,
+    stat: &GaussianSuffStat,
+) -> NormalInvGamma {
+    let n = stat.n() as f64;
+
+    let (m, v, a, b) = nig.params();
+
+    let v_inv = v.recip();
+
+    let vn_inv = v_inv + n;
+    let vn = vn_inv.recip();
+    let mn = (v_inv * m + stat.sum_x()) * vn;
+    let an = a + 0.5 * n;
+    let bn = b + 0.5 * (m * m * v_inv + stat.sum_x_sq() - mn * mn * vn_inv);
+
+    NormalInvGamma::new(mn, vn, an, bn).expect("Invalid posterior params.")
+}
+
+impl ConjugatePrior<f64, Gaussian> for NormalInvGamma {
+    type Posterior = Self;
+    type LnMCache = f64;
+    type LnPpCache = (GaussianSuffStat, f64);
+
+    fn posterior(&self, x: &DataOrSuffStat<f64, Gaussian>) -> Self {
+        extract_stat_then!(x, |stat: &GaussianSuffStat| {
+            posterior_from_stat(&self, &stat)
+        })
+    }
+
+    #[inline]
+    fn ln_m_cache(&self) -> Self::LnMCache {
+        ln_z(self.v, self.a, self.b)
+    }
+
+    fn ln_m_with_cache(
+        &self,
+        cache: &Self::LnMCache,
+        x: &DataOrSuffStat<f64, Gaussian>,
+    ) -> f64 {
+        extract_stat_then!(x, |stat: &GaussianSuffStat| {
+            let post = posterior_from_stat(&self, &stat);
+            let n = stat.n() as f64;
+            let lnz_n = ln_z(post.v, post.a, post.b);
+            lnz_n - cache - 0.5 * n * LN_PI - n * LN_2
+        })
+    }
+
+    #[inline]
+    fn ln_pp_cache(
+        &self,
+        x: &DataOrSuffStat<f64, Gaussian>,
+    ) -> Self::LnPpCache {
+        let stat = extract_stat(&x);
+        let post_n = posterior_from_stat(&self, &stat);
+        let lnz_n = ln_z(post_n.v, post_n.a, post_n.b);
+        (stat, lnz_n)
+    }
+
+    fn ln_pp_with_cache(&self, cache: &Self::LnPpCache, y: &f64) -> f64 {
+        let mut stat = cache.0.clone();
+        let lnz_n = cache.1;
+
+        stat.observe(y);
+        let post_m = posterior_from_stat(&self, &stat);
+
+        let lnz_m = ln_z(post_m.v, post_m.a, post_m.b);
+
+        HALF_LN_PI + LN_2 + lnz_m - lnz_n
+    }
+}
+
+fn extract_stat(x: &DataOrSuffStat<f64, Gaussian>) -> GaussianSuffStat {
+    match x {
+        DataOrSuffStat::SuffStat(ref s) => (*s).clone(),
+        DataOrSuffStat::Data(xs) => {
+            let mut stat = GaussianSuffStat::new();
+            xs.iter().for_each(|y| stat.observe(y));
+            stat
+        }
+        DataOrSuffStat::None => GaussianSuffStat::new(),
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const TOL: f64 = 1E-12;
+
+    // XXX: Implemented this directly against the Kevin Murphy whitepaper. Makes
+    // things a little easier to understand compared to using the sufficient
+    // statistics and traits and all that. Still possible for this to be wrong,
+    // but if this matches what's in the code AND the DPGMM example (at
+    // examples/dpgmm.rs) words with the NormalInvGamma prior, then we should be
+    // good to go.
+    fn alternate_ln_marginal(
+        xs: &Vec<f64>,
+        m: f64,
+        v: f64,
+        a: f64,
+        b: f64,
+    ) -> f64 {
+        let n = xs.len() as f64;
+        let sum_x: f64 = xs.iter().sum();
+        let sum_x_sq: f64 = xs.iter().map(|&x| x * x).sum();
+
+        let v_inv = v.recip();
+        let vn_inv = v_inv + n;
+        let vn = vn_inv.recip();
+        let mn = (v_inv * m + sum_x) * vn;
+        let an = a + n / 2.0;
+        let bn = b + 0.5 * (m * m * v_inv + sum_x_sq - mn * mn * vn_inv);
+
+        let numer = 0.5 * vn.ln() + a * b.ln() + an.ln_gamma().0;
+        let denom = 0.5 * v.ln()
+            + an * bn.ln()
+            + a.ln_gamma().0
+            + (n / 2.0) * LN_PI
+            + n * LN_2;
+
+        numer - denom
+    }
+
+    #[test]
+    fn ln_m_vs_reference() {
+        let (m, v, a, b) = (0.0, 1.0, 1.0, 1.0);
+        let xs = vec![1.0, 2.0, 3.0, 4.0, 5.0];
+        let reference = alternate_ln_marginal(&xs, m, v, a, b);
+        let nig = NormalInvGamma::new(m, v, a, b).unwrap();
+        let ln_m = nig.ln_m(&DataOrSuffStat::<f64, Gaussian>::from(&xs));
+
+        assert::close(reference, ln_m, TOL);
+    }
+}
