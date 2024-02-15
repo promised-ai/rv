fd74ea17f2b1e19e4de0971b80f31babbaeaa562
Chad Scherrer <chad.scherrer@gmail.com>
2024-02-15 10:30:02 -0800

    bring sbd into scope

--- src/experimental/mod.rs
+++ src/experimental/mod.rs
@@ -1,7 +1,9 @@
+mod sbd;
 mod stick_breaking;
 mod stick_breaking_stat;
 mod stick_sequence;
 
+pub use sbd::Sbd;
 pub use stick_breaking::{StickBreaking, StickBreakingError};
 pub use stick_breaking_stat::StickBreakingSuffStat;
 pub use stick_sequence::StickSequence;
