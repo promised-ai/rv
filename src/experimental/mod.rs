54daace1ba4f3724e54a8b96941b7a32e6f25f1b
Chad Scherrer <chad.scherrer@gmail.com>
2024-02-16 15:40:59 -0800

    work on sbd_stat

--- src/experimental/mod.rs
+++ src/experimental/mod.rs
@@ -1,9 +1,11 @@
 mod sbd;
+mod sbd_stat;
 mod stick_breaking;
 mod stick_breaking_stat;
 mod stick_sequence;
 
 pub use sbd::Sbd;
+pub use sbd_stat::SbdSuffStat;
 pub use stick_breaking::{StickBreaking, StickBreakingError};
 pub use stick_breaking_stat::StickBreakingSuffStat;
 pub use stick_sequence::StickSequence;
---
+++ src/experimental/sbd_stat.rs
@@ -0,0 +1,62 @@
+use crate::experimental::sbd::Sbd;
+use crate::{
+    data::UnitPowerLawSuffStat,
+    suffstat_traits::{HasSuffStat, SuffStat},
+};
+#[cfg(feature = "serde1")]
+use serde::{Deserialize, Serialize};
+
+#[cfg_attr(feature = "serde1", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "serde1", serde(rename_all = "snake_case"))]
+#[derive(Clone, Debug, PartialEq)]
+pub struct SbdSuffStat {
+    pub counts: Vec<usize>
+}
+
+impl Default for SbdSuffStat {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl SbdSuffStat {
+    pub fn new() -> Self {
+        Self {
+            counts: Vec::new()
+        }
+    }
+}
+
+
+
+impl HasSuffStat<usize> for Sbd {
+    type Stat = SbdSuffStat;
+
+    fn empty_suffstat(&self) -> Self::Stat {
+        Self::Stat::new()
+    }
+
+    fn ln_f_stat(&self, stat: &Self::Stat) -> f64 {
+        todo!()
+    }
+}
+
+impl SuffStat<usize> for SbdSuffStat {
+    fn n(&self) -> usize {
+        self.counts.iter().sum()
+    }
+
+    fn observe(&mut self, i: &usize) {
+        if self.counts.len() < *i + 1 {
+            self.counts.resize(*i + 1, 0)
+        }
+        self.counts[*i] += 1;
+        
+    }
+
+    fn forget(&mut self, i: &usize) {
+        assert!(self.counts[*i] > 0, "No observations of {i} to forget.");
+        self.counts[*i] -= 1;
+    }
+}
+
