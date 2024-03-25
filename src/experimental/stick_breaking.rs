630c20d37957ac239a0f2a7a39cc16053c16cd0f
Chad Scherrer <chad.scherrer@gmail.com>
2024-03-25 15:16:51 -0700

    simplify

--- src/experimental/stick_breaking.rs
+++ src/experimental/stick_breaking.rs
@@ -247,9 +247,9 @@ mod tests {
     #[test]
     fn sb_repeated_obs_more_likely() {
         let sb = StickBreaking::new(UnitPowerLaw::new(5.0).unwrap());
-        let sb_m = sb.m(&DataOrSuffStat::Data(&vec![1, 2, 2, 2]));
-        let post = sb.posterior(&DataOrSuffStat::Data(&vec![1, 2, 2, 2]));
-        let post_m = post.m(&DataOrSuffStat::Data(&vec![1, 2, 2, 2]));
+        let sb_m = sb.m(&DataOrSuffStat::Data(&vec![1, 2]));
+        let post = sb.posterior(&DataOrSuffStat::Data(&vec![1, 2]));
+        let post_m = post.m(&DataOrSuffStat::Data(&vec![1, 2]));
         assert!(post_m > sb_m);
     }
 } // mod tests
