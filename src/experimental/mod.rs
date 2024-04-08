b281a045190e607de55ee58e763b2c90afe69fa9
Chad Scherrer <chad.scherrer@gmail.com>
2024-04-08 12:57:10 -0700

    file reorg

--- src/experimental/mod.rs
+++ src/experimental/mod.rs
@@ -1,11 +1,1 @@
-pub mod sbd;
-pub mod sbd_stat;
 pub mod stick_breaking;
-// pub mod stick_breaking_stat;
-pub mod stick_sequence;
-
-pub use sbd::StickBreakingDiscrete;
-pub use sbd_stat::StickBreakingDiscreteSuffStat;
-pub use stick_breaking::StickBreaking;
-// pub use stick_breaking_stat::*;
-pub use stick_sequence::StickSequence;
