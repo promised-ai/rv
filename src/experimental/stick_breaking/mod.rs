f69f3902d2cc14085a54074923d14d6b6236f259
Chad Scherrer <chad.scherrer@gmail.com>
2024-04-09 10:19:25 -0700

    cargo fmt

--- src/experimental/stick_breaking/mod.rs
+++ src/experimental/stick_breaking/mod.rs
@@ -4,9 +4,8 @@ pub mod stick_breaking;
 pub mod stick_breaking_stat;
 pub mod stick_sequence;
 
-
 pub use sbd::StickBreakingDiscrete;
 pub use sbd_stat::StickBreakingDiscreteSuffStat;
-pub use stick_breaking::{StickBreaking, BreakSequence, PartialWeights};
+pub use stick_breaking::{BreakSequence, PartialWeights, StickBreaking};
 // pub use stick_breaking_stat::*;
 pub use stick_sequence::StickSequence;
