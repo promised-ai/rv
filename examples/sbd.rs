29687cd9d7bcd025eff2b093081b67ee4ab45836
Chad Scherrer <chad.scherrer@gmail.com>
2024-04-11 16:05:27 -0700

    update sbd example

--- examples/sbd.rs
+++ examples/sbd.rs
@@ -12,7 +12,7 @@ fn main() {
     #[cfg(feature = "experimental")]
     {
         // Instantiate a stick-breaking process
-        let alpha = 5.0;
+        let alpha = 100.0;
         let sbp = StickBreaking::new(UnitPowerLaw::new(alpha).unwrap());
 
         // Sample from it to get a StickSequence
@@ -21,29 +21,39 @@ fn main() {
         // Use the StickSequence to instantiate a stick-breaking discrete distribution
         let sbd = StickBreakingDiscrete::new(sticks.clone());
 
-        // Now sample from the StickBreakingDiscrete and find its sufficient statistic
-        let n = 10000;
-        let xs = sbd.sample(n, &mut rand::thread_rng());
-        let stat = StickBreakingDiscreteSuffStat::from(&xs[..]);
+        
+        let start = std::time::Instant::now();
+        let entropy = sbd.entropy();
+        let duration = start.elapsed();
+        println!("Entropy: {}", entropy);
+        println!("Time elapsed in entropy() is: {:?}", duration);
 
-        // Use the sufficient statistic to find the posterior
-        let post = sbp.posterior_from_suffstat(&stat);
+        let num_weights = sbd.stick_sequence().num_weights_unstable();
 
-        let mut break_iter = post.break_dists();
+        println!("num weights: {}", num_weights);
+        // // Now sample from the StickBreakingDiscrete and find its sufficient statistic
+        // let n = 10000;
+        // let xs = sbd.sample(n, &mut rand::thread_rng());
+        // let stat = StickBreakingDiscreteSuffStat::from(&xs[..]);
 
-        // Print the posterior parameters of each Beta distribution.
-        while let Some(Either::Left(p)) = break_iter.next() {
-            let mean: f64 = p.mean().unwrap();
-            println!(
-                "alpha: {}\t beta: {}\t mean: {}",
-                p.alpha(),
-                p.beta(),
-                mean
-            );
-        }
+        // // Use the sufficient statistic to find the posterior
+        // let post = sbp.posterior_from_suffstat(&stat);
 
-        let cache = sbp.ln_pp_cache(&DataOrSuffStat::SuffStat(&stat));
-        let sum: f64 = (0..99).map(|y| sbp.pp_with_cache(&cache, &y)).sum();
-        println!("P(posterior predictive < 100) {}", sum);
+        // let mut break_iter = post.break_dists();
+
+        // // Print the posterior parameters of each Beta distribution.
+        // while let Some(Either::Left(p)) = break_iter.next() {
+        //     let mean: f64 = p.mean().unwrap();
+        //     println!(
+        //         "alpha: {}\t beta: {}\t mean: {}",
+        //         p.alpha(),
+        //         p.beta(),
+        //         mean
+        //     );
+        // }
+
+        // let cache = sbp.ln_pp_cache(&DataOrSuffStat::SuffStat(&stat));
+        // let sum: f64 = (0..99).map(|y| sbp.pp_with_cache(&cache, &y)).sum();
+        // println!("P(posterior predictive < 100) {}", sum);
     }
 }
