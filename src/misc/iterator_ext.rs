3c280806ce3d4bbe1027c8683b53ec5885ca7688
Chad Scherrer <chad.scherrer@gmail.com>
2024-04-11 14:33:34 -0700

    update

--- src/misc/iterator_ext.rs
+++ src/misc/iterator_ext.rs
@@ -1,6 +1,6 @@
 use core::iter::Map;
+use itertools::Itertools;
 use itertools::TupleWindows;
-use itertools::{iterate, Itertools};
 
 /// Provides extension methods for iterators over `f64` values.
 pub trait IteratorExt: Iterator<Item = f64> + Sized {
@@ -8,16 +8,20 @@ pub trait IteratorExt: Iterator<Item = f64> + Sized {
         self,
     ) -> Map<TupleWindows<Self, (f64, f64, f64)>, fn((f64, f64, f64)) -> f64>
     {
-        self.tuple_windows::<(_, _, _)>().map(|(x, x2, x3)| {
-            let dx = x2 - x;
-            let dx2 = x3 - x2;
-            let ddx = dx2 - dx;
-            x - dx.powi(2) / ddx
+        self.tuple_windows::<(_, _, _)>().map(|(x, y, z)| {
+            let dx = z - y;
+            let dx2 = y - x;
+            let ddx = dx - dx2;
+            z - dx.powi(2) / ddx
         })
     }
 
     fn limit(self, tol: f64) -> f64 {
-        self.tuple_windows::<(_, _)>()
+        self.aitken()
+            .aitken()
+            .aitken()
+            .aitken()
+            .tuple_windows::<(_, _)>()
             .filter_map(
                 |(a, b)| {
                     if (a - b).abs() < tol {
@@ -44,15 +48,16 @@ mod tests {
         let seq = (0..)
             .map(|n| {
                 let sign = if n.is_even() { 1.0 } else { -1.0 };
-
-                sign / (2 * n + 1) as f64
+                let val = sign / (2 * n + 1) as f64;
+                dbg!(val);
+                val
             })
             .scan(0.0, |acc, x| {
                 *acc += x;
                 Some(*acc)
             });
-        let limit = seq.aitken().limit(1e-5);
+        let limit = seq.limit(1e-10);
         let pi_over_4 = std::f64::consts::PI / 4.0;
-        assert!((limit - pi_over_4).abs() < 1e-5, "The limit calculated using Aitken's Δ² process did not converge to π/4 within the tolerance.");
+        assert!((limit - pi_over_4).abs() < 1e-10, "The limit calculated using Aitken's Δ² process did not converge to π/4 within the tolerance.");
     }
 }
