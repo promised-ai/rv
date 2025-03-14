75956f1841379263f9d98a30f956bb1ad3f2c03a
Chad Scherrer <chad.scherrer@gmail.com>
2025-03-14 09:58:32 -0700

    crate::impl_shiftable!

--- src/dist/beta.rs
+++ src/dist/beta.rs
@@ -398,8 +398,7 @@ macro_rules! impl_traits {
     };
 }
 
-use crate::impl_shiftable;
-impl_shiftable!(Beta);
+crate::impl_shiftable!(Beta);
 
 
 impl Variance<f64> for Beta {
@@ -472,7 +471,6 @@ mod tests {
     use super::*;
     use crate::misc::ks_test;
     use crate::test_basic_impls;
-    use crate::test_shiftable;
     use proptest::prelude::*;
 
     const TOL: f64 = 1E-12;
@@ -481,7 +479,7 @@ mod tests {
 
     test_basic_impls!(f64, Beta, Beta::jeffreys());
 
-    test_shiftable!(Beta::new(1.0, 2.0).unwrap());
+    crate::test_shiftable!(Beta::new(1.0, 2.0).unwrap());
 
     #[test]
     fn new() {
