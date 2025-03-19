739bb386338f6748770240e8c92137864acfc3a5
Chad Scherrer <chad.scherrer@gmail.com>
2025-03-19 11:26:31 -0700

    drop unused imports

--- src/dist/scaled_inv_chi_squared.rs
+++ src/dist/scaled_inv_chi_squared.rs
@@ -612,8 +612,6 @@ mod test {
 
     use crate::test_scalable_cdf;
     use crate::test_scalable_density;
-    use crate::test_scalable_entropy;
-    use crate::test_scalable_invcdf;
     use crate::test_scalable_method;
 
     test_scalable_method!(ScaledInvChiSquared::new(2.0, 4.0).unwrap(), mean);
